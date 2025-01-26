4c579d035fee4ceaae6b74fd3e0f6087dd8f280c
dung204 <acezombiev4@gmail.com>
2025-01-26 11:19:29 +0700

    refactor: use base model

--- src/modules/user/models/user.model.ts
+++ src/modules/user/models/user.model.ts
@@ -1,20 +1,17 @@
-import { randomUUID } from 'crypto';
 import { Schema, model } from 'mongoose';
 
+import { BaseModel, baseModelSchemaDefinition } from '@/base/common/models';
 import { Role } from '@/modules/user/enums/role.enum';
 
-export interface User {
-  _id: string;
+export interface User extends BaseModel {
   firstName: string;
   lastName: string;
   address: string | null;
   role: Role;
-  createTimestamp: Date;
-  deleteTimestamp: Date | null;
 }
 
 const userSchema = new Schema<User>({
-  _id: { type: String, default: randomUUID, required: true },
+  ...baseModelSchemaDefinition,
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   address: { type: String, default: null, required: false },
@@ -24,8 +21,6 @@ const userSchema = new Schema<User>({
     default: Role.USER,
     required: false,
   },
-  createTimestamp: { type: Date, default: Date.now, required: true },
-  deleteTimestamp: { type: Date, default: null, required: false },
 });
 
 export const UserModel = model<User>('User', userSchema);

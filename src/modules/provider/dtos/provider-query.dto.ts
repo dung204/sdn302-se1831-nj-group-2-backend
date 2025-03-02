0f2c305c13929d7a53b9b65e516edc098c92d849
dung204 <acezombiev4@gmail.com>
2025-03-02 20:56:10 +0700

    refactor(provider): remove `description` sorting field

--- src/modules/provider/dtos/provider-query.dto.ts
+++ src/modules/provider/dtos/provider-query.dto.ts
@@ -8,7 +8,6 @@ export const providerQueryDto = commonQueryDto
     sorting: SortingUtils.getSortingValueSchema([
       'id',
       'name',
-      'description',
       'createTimestamp',
       'deleteTimestamp',
     ]),

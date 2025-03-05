5876e753768df7db138668a3c7658252665d20e6
dung204 <acezombiev4@gmail.com>
2025-03-05 21:20:31 +0700

    refactor: remove 'description' from service category query DTO sorting

--- src/modules/service-category/dtos/service-category-query.dto.ts
+++ src/modules/service-category/dtos/service-category-query.dto.ts
@@ -8,7 +8,6 @@ export const serviceCategoryQueryDto = commonQueryDto
     sorting: SortingUtils.getSortingValueSchema([
       'id',
       'name',
-      'description',
       'createTimestamp',
       'deleteTimestamp',
     ]),

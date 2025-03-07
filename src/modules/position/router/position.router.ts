b12fff9e94ff2c8780e7019250966fd49bca95a4
Anh <anhnnhe176642@fpt.edu.vn>
2025-03-07 19:13:48 +0700

    feat(position): update AuthGuard roles to include BRANCH_ADMIN

--- src/modules/position/router/position.router.ts
+++ src/modules/position/router/position.router.ts
@@ -8,36 +8,36 @@ export const positionRouter = Router();
 
 positionRouter.get(
   '/',
-  AuthGuard([Role.ADMIN, Role.STAFF, Role.OWNER]),
+  AuthGuard([Role.BRANCH_ADMIN, Role.STAFF, Role.OWNER]),
   positionController.findAll,
 );
 positionRouter.get(
   '/deleted',
-  AuthGuard([Role.ADMIN, Role.OWNER]),
+  AuthGuard([Role.BRANCH_ADMIN, Role.OWNER]),
   positionController.findAllDeleted,
 );
 positionRouter.get(
   '/:id',
-  AuthGuard([Role.ADMIN, Role.STAFF, Role.OWNER]),
+  AuthGuard([Role.BRANCH_ADMIN, Role.STAFF, Role.OWNER]),
   positionController.findOneById,
 );
 positionRouter.post(
   '/',
-  AuthGuard([Role.ADMIN, Role.OWNER]),
+  AuthGuard([Role.BRANCH_ADMIN, Role.OWNER]),
   positionController.createPosition,
 );
 positionRouter.patch(
   '/:id',
-  AuthGuard([Role.ADMIN, Role.OWNER]),
+  AuthGuard([Role.BRANCH_ADMIN, Role.OWNER]),
   positionController.updatePosition,
 );
 positionRouter.delete(
   '/:id',
-  AuthGuard([Role.ADMIN, Role.STAFF, Role.OWNER]),
+  AuthGuard([Role.BRANCH_ADMIN, Role.STAFF, Role.OWNER]),
   positionController.softDeletePosition,
 );
 positionRouter.patch(
   '/restore/:id',
-  AuthGuard([Role.ADMIN, Role.OWNER]),
+  AuthGuard([Role.BRANCH_ADMIN, Role.OWNER]),
   positionController.restorePosition,
 );

748777e1ece653e33e39b3d8f96005528c77a8a1
dung204 <acezombiev4@gmail.com>
2025-03-09 10:46:32 +0700

    feat: include filter in success response body type

--- src/base/common/types/response-body.types.ts
+++ src/base/common/types/response-body.types.ts
@@ -2,8 +2,15 @@ import { HttpStatusCode } from '@/base/common/enums';
 import { Pagination, Sorting } from '@/base/common/types';
 
 export type SuccessResponseBody<T> = T extends unknown[]
-  ? { data: T; meta: { pagination: Pagination; sorting: Sorting[] } }
-  : { data: T };
+  ? {
+      data: T;
+      meta: {
+        pagination: Pagination;
+        sorting: Sorting[];
+        filter?: Record<string, unknown>;
+      };
+    }
+  : { data: T; meta?: { filter?: Record<string, unknown> } };
 
 export type FailedResponseBody = {
   messages?: string[];
